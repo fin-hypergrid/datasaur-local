6a4a54af1699f513ee645af311cc04a2e0127b7c
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2017-12-06 22:46:12 -0500

    moved treeColumnIndex, rowColumnIndex to Behavior.js

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ var DataSourceBase = require('datasaur-base');
 var getFieldNames = require('fin-hypergrid-field-tools').getFieldNames;
 
 
+/** @typedef {object} columnSchemaObject
+ * @property {string} name - The required column name.
+ * @property {string} [header] - An override for derived header
+ * @property {function} [calculator] - A function for a computed column. Undefined for normal data columns.
+ * @property {string} [type] - Used for sorting when and only when comparator not given.
+ * @property {object} [comparator] - For sorting, both of following required:
+ * @property {function} comparator.asc - ascending comparator
+ * @property {function} comparator.desc - descending comparator
+ */
+
+
 /**
  * See {@link DataSourceOrigin#initialize} for constructor parameters.
  * @constructor
@@ -14,26 +25,16 @@ var getFieldNames = require('fin-hypergrid-field-tools').getFieldNames;
 var DataSourceLocal = DataSourceBase.extend('DataSourceLocal',  {
 
     initialize: function(data, schema, options) {
-        delete this.dataSource; // added by DataSourceBase#initialize but we don't want here
-
-        this.treeColumnIndex = options.treeColumnIndex;
-        this.rowColumnIndex = options.rowColumnIndex;
-
-        this.schema = setInternalColSchema.call(this, []);
+        /**
+         * @summary The array of column schema objects.
+         * @name schema
+         * @type {columnSchemaObject[]}
+         * @memberOf DataSourceLocal#
+         */
+        this.schema = [];
 
         this.setData(data, schema);
     },
-
-
-    /** @typedef {object} columnSchemaObject
-     * @property {string} name - The required column name.
-     * @property {string} [header] - An override for derived header
-     * @property {function} [calculator] - A function for a computed column. Undefined for normal data columns.
-     * @property {string} [type] - Used for sorting when and only when comparator not given.
-     * @property {object} [comparator] - For sorting, both of following required:
-     * @property {function} comparator.asc - ascending comparator
-     * @property {function} comparator.desc - descending comparator
-     */
 
     /**
      * Establish a new data and schema.
@@ -46,8 +47,8 @@ var DataSourceLocal = DataSourceBase.extend('DataSourceLocal',  {
     setData: function(data, schema) {
         /**
          * @summary The array of uniform data objects.
-         * @name schema
-         * @type {columnSchemaObject[]}
+         * @name data
+         * @type {object[]}
          * @memberOf DataSourceLocal#
          */
         this.data = data || [];
@@ -60,109 +61,122 @@ var DataSourceLocal = DataSourceBase.extend('DataSourceLocal',  {
     },
 
     /**
-     * @memberOf DataSourceLocal#
      * @returns {columnSchemaObject[]}
+     * @memberOf DataSourceLocal#
      */
     getSchema:  function(){
         return this.schema;
     },
     /**
-     * @memberOf DataSourceLocal#
      * Caveat: Do not call on a data update when you expect to reuse the existing schema.
      * @param schema
+     * @memberOf DataSourceLocal#
      */
     setSchema: function(schema){
         if (!schema.length) {
-            var fields = getFieldNames(this.data[0]);
+            var dataRow = this.data.find(function(dataRow) { return dataRow; });
 
-            schema = Array(fields.length);
-
-            for (var i = 0; i < fields.length; i++) {
-                schema[i] = { name: fields[i] };
-            }
+            schema = getFieldNames(dataRow).map(function(name) {
+                return { name: name };
+            });
         }
 
-        /**
-         * @summary The array of column schema objects.
-         * @name schema
-         * @type {columnSchemaObject[]}
-         * @memberOf DataSourceLocal#
-         */
-        this.schema = setInternalColSchema.call(this, schema);
-    },
-
-    isNullObject: false,
-
-    getDataIndex: function(y) {
-        return y;
+        this.schema = schema;
     },
 
     /**
-     * @memberOf DataSourceLocal#
      * @param y
      * @returns {dataRowObject}
+     * @memberOf DataSourceLocal#
      */
     getRow: function(y) {
         return this.data[y];
     },
 
-    addRow: function(dataRow) {
-        this.data.push(dataRow);
-    },
-
-    delRow: function(y) {
-        return this.data.splice(y, 1);
+    /**
+     * Update or blank row in place.
+     *
+     * _Note parameter order is the reverse of `addRow`._
+     * @param {number} y
+     * @param {object} [dataRow] - if omitted or otherwise falsy, row renders as blank
+     * @memberOf DataSourceLocal#
+     */
+    setRow: function(y, dataRow) {
+        this.data[y] = dataRow || undefined;
     },
 
     /**
+     * Insert or append a new row.
+     *
+     * _Note parameter order is the reverse of `setRow`._
+     * @param {object} dataRow
+     * @param {number} [y=Infinity] - The index of the new row. If `y` >= row count, row is appended to end; otherwise row is inserted at `y` and row indexes of all remaining rows are incremented.
      * @memberOf DataSourceLocal#
-     * @param x
-     * @param y
+     */
+    addRow: function(dataRow, y) {
+        if (y === undefined || y >= this.getRowCount()) {
+            this.data.push(dataRow);
+        } else {
+            this.data.splice(y, 0, dataRow);
+        }
+    },
+
+    /**
+     * Rows are removed entirely and no longer render.
+     * Indexes of all remaining rows are decreased by `rowCount`.
+     * @param {number} y
+     * @param {number} [rowCount=1]
+     * @returns {dataRowObject[]}
+     * @memberOf DataSourceLocal#
+     */
+    delRow: function(y, rowCount) {
+        if (rowCount === undefined) { rowCount = 1; }
+        return this.data.splice(y, rowCount);
+    },
+
+    /**
+     * @param {number} x
+     * @param {number} y
      * @returns {*}
+     * @memberOf DataSourceLocal#
      */
     getValue: function(x, y) {
         var row = this.getRow(y);
         if (!row) {
             return null;
         }
-        return row[this.getColumnName(x)];
+        return row[getColumnName.call(this, x)];
     },
 
     /**
-     * @memberOf DataSourceLocal#
      * @param {number} x
      * @param {number} y
      * @param value
+     * @memberOf DataSourceLocal#
      */
     setValue: function(x, y, value) {
-        this.getRow(y)[this.getColumnName(x)] = value;
-    },
-
-    getColumnName: function(x) {
-        return (typeof x)[0] === 'n' ? this.schema[x].name : x;
+        this.getRow(y)[getColumnName.call(this, x)] = value;
     },
 
     /**
-     * @memberOf DataSourceLocal#
      * @returns {number}
+     * @memberOf DataSourceLocal#
      */
     getRowCount: function() {
         return this.data.length;
     },
 
     /**
-     * @memberOf DataSourceLocal#
      * @returns {number}
+     * @memberOf DataSourceLocal#
      */
     getColumnCount: function() {
         return this.schema.length;
     }
 });
 
-function setInternalColSchema(schema) {
-    schema[this.treeColumnIndex] = schema[this.treeColumnIndex] || { name: 'Tree', header: 'Tree' };
-    schema[this.rowColumnIndex] = schema[this.rowColumnIndex] || { name: '', header: '' };
-    return schema;
+function getColumnName(x) {
+    return (typeof x)[0] === 'n' ? this.schema[x].name : x;
 }
 
 
